4ff38d62bc4e2f5101981468069080027f47ad41
Souza <38145379+misael28@users.noreply.github.com>
2021-08-15 01:27:39 -0300

    chore: add database and typeorm config

---
+++ src/database/migrations/1629000577343-CreateUsers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUsers1629000577343 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}

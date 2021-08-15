f3d43f7cca621e781292178dbbfa6cd2d6e4b9dc
Souza <38145379+misael28@users.noreply.github.com>
2021-08-15 12:40:55 -0300

    feat: add error treatement at server

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
 import "reflect-metadata";
 import "./database";
 import { router } from "./routes";
@@ -7,4 +8,17 @@ const app = express();
 app.use(express.json());
 app.use(router);
 
-app.listen(3000, () => console.log("server is running bitch!"));
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(404).json({
+      error: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal Server Error",
+  });
+});
+
+app.listen(3000, () => console.log("Server is running!"));

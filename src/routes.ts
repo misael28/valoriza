59eee4f557b1a9e5c5be6641327b6b24a4e63b19
Souza <38145379+misael28@users.noreply.github.com>
2021-08-20 22:53:22 -0300

    feat: add create tag and basic auth

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { CreateUserController } from "../src/controllers/createUser";
 import { CreateTagController } from "./controllers/createTag";
-
+import { ensureAdmin } from "../src/middlewares/ensureAdmin";
 const router = Router();
 const createUser = new CreateUserController();
 const createTagController = new CreateTagController();
 
 router.post("/users", createUser.handle);
-router.post("/tags", createTagController.handle);
+router.post("/tags", ensureAdmin, createTagController.handle);
 export { router };
